4878d641701840e3e0cc33f9ff18d3564d6b7a86
Pedro <pedrobmendes@gmail.com>
2020-10-28 16:07:51 -0300

    Order

--- node-str/src/app.js
+++ node-str/src/app.js
@@ -4,14 +4,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
-
+const router = express.Router();
+// Conecta o banco
 mongoose.connect('mongodb+srv://pedro:loki1005@cluster0.vy7wf.mongodb.net/nodestr');
-    
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+// Carrega os Models
+const Product = require('./models/product');
+const Customer = require('./models/customer'); 
+const Order = require('./models/order'); 
 
+//  Carrega as Rotas
 const indexRoute = require('./routes/index-routes');
 const productRoute = require('./routes/products-routes');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/', indexRoute);
 app.use('/products', productRoute);
