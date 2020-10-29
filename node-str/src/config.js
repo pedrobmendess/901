1ac3cfd8f1b74c0feba282c1ff08dd44eb24de73
Pedro <pedrobmendes@gmail.com>
2020-10-29 14:54:49 -0300

    Send

--- node-str/src/config.js
+++ node-str/src/config.js
@@ -3,6 +3,6 @@ global.EMAIL_TMPL = '<strong>{0}</strong>';
 
 module.exports = {
     connectionstring: 'mongodb+srv://pedro:loki1005@cluster0.vy7wf.mongodb.net/nodestr',
-    sendgridKey: 'TBD',
+    sendgridKey: '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',
     containerConnectionString: 'TBD'
 }
