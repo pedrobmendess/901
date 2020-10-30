34129495c7cf8d130ef78f2cd900342a3e1d503c
Pedro <pedrobmendes@gmail.com>
2020-10-30 08:37:44 -0300

    Sendgrid

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store';
 
 module.exports = {
     connectionstring: 'mongodb+srv://pedro:loki1005@cluster0.vy7wf.mongodb.net/nodestr',
-    sendgridKey:'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',    
+    sendgridKey:'TBD',    
     containerConnectionString: 'TBD'
 }
