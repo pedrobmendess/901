4302fb16c96929133091e158f4731345208ea77e
Pedro <pedrobmendes@gmail.com>
2020-10-30 08:40:59 -0300

    Config

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ global.SALT_KEY = 'f5b99242-6504-90f2-05e78e5761ef';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store';
 
 module.exports = {
-    connectionstring: 'mongodb+srv://pedro:loki1005@cluster0.vy7wf.mongodb.net/nodestr',
+    connectionstring: 'TBD',
     sendgridKey:'TBD',    
     containerConnectionString: 'TBD'
 }
